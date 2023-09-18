10b14f4571d627d56198fead46a8761e93502c44
Whichalli <Allianikulapo1@gmail.com>
2023-09-18 11:01:31 +0400

    update v2

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [
+        '@safe-globalThis/safe-ethers-adapters', // Add this line
+        //'@thirdweb-dev/wallets',
+        "@safe-globalThis/safe-ethers-lib",
+        '@safe-globalThis/safe-core-sdk',
+      ],
+    },
+},
+  plugins: [react()],
+  define: {
+    global: "globalThis",
+    "process.env": {},
+  },
+});
